237cc177fa1a75555d3ea6f1d45bd5e6ce1c75d5
Onuwa Chinedu Joseph <onuwachinedu66@gmail.com>
2023-02-27 23:50:41 -0800

    Segmentation erro fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index;
 
-	while (s[index++])
-		len++;
+	for (index = 0; s[index] != '\0'; index++) 
+		;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index--; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
